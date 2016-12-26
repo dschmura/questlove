f9962b103751b024022b040804b3700cf488d7bc
David Chmura <dschmura@humbledaisy.com>
2016-12-26 12:48:32 -0500

    Update @site_name to be 'QuestLove'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper  
   def site_name
-    @site_name = ''
+    @site_name = 'QuestLove'
     @site_name = @site_name || Rails.application.class.parent_name.gsub(/[A-Z]/)  { |c| " #{c} "} 
   end
 
